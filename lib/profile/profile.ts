31e9340aaef1827295f9a155c778b7bde8e85ed3
Petr Bažout <bazout.p@gmail.com>
2016-03-04 16:41:17 +0100

    Comment interfaces for profile

--- lib/profile/profile.ts
+++ lib/profile/profile.ts
@@ -2,21 +2,65 @@
 import CSCoreSDK = require('cs-core-sdk');
 
 export interface Profile {
+    
+    /**
+    * user's first name
+    */
     firstName: string,
+    
+    /**
+    * user's last name
+    */
     lastName: string,
+    
+    /**
+    * user's name used for salutation
+    */
     salutation?: string,
+    
+    /**
+    * customer's id
+    */
     customerId: string,
+    
+    /**
+    * number of institute
+    */
     institudeId: number,
+    
+    /**
+    * Has the customer approved §107 telecommunication act. Possible values: ACCEPTED, NOT_ACCEPTED, UNKNOWN.
+    */
     marketingInfoAcceptance: string,
+    
+    /**
+    * user's gender. Possible values: MALE, FEMALE, UNKNOWN.
+    */
     gender: string,
+    
+    /**
+    * Date and time of the last login of customer. Common last login for all client applications - George, QC, etc.
+    */
     lastlogin?: Date
 }
 
 export interface LastLogin {
+    
+    /**
+    * Description of profile login info
+    */
     lastlogin?: [LastLoginInfo]
 }
 
 export interface LastLoginInfo {
+    
+    /**
+    * Channel of the last login.
+    */
     channel: string,
+    
+    /**
+    * Date of the last login.
+    */
     lastlogin: Date
 }
