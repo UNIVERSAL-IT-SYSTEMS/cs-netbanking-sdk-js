208676c8ad166ff41f4928a4f32056b57e12eb0d
Petr Bažout <bazout.p@gmail.com>
2016-03-10 11:39:08 +0100

    Add transactions resource

--- lib/cards/transactions.ts
+++ lib/cards/transactions.ts
@@ -1,3 +1,21 @@
 /// <reference path="../../node_modules/cs-core-sdk/dist/cs-core-sdk.node.d.ts" />
 import CSCoreSDK = require('cs-core-sdk');
 import {TransactionList, AddNoteAndMarkTransactionsRequest} from '../common';
+
+export class CardTransactionsResource extends CSCoreSDK.Resource
+implements CSCoreSDK.HasInstanceResource<CardTransactionResource> {
+    
+    withId = (id: string) : CardTransactionResource => {
+        return new CardTransactionResource(id, this.getPath(), this._client);
+    }
+    
+    // zkontrolovat, pravděpodobně nebude fungovat
+    export = (params) : Promise<> => {
+        return CSCoreSDK.ResourceUtils.CallCreateWithSuffix(this, 'export', params);
+    }
+}
+
+export class CardTransactionResource extends CSCoreSDK.InstanceResource
+implements CSCoreSDK.UpdateEnabled<AddNoteAndMarkTransactionsRequest, TransactionList> {
+    
+}
