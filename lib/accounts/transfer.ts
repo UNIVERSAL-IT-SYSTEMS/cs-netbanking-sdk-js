e48bbd2d600fb4824d71bd65b8ab848035847bfe
Petr Bažout <bazout.p@gmail.com>
2016-04-29 17:13:33 +0200

    Add Signing support for accounts transfer

--- lib/accounts/transfer.ts
+++ lib/accounts/transfer.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../node_modules/cs-core-sdk/dist/cs-core-sdk.node.d.ts" />
 import CSCoreSDK = require('cs-core-sdk');
-import {Signable, Amount} from '../common';
+import {Amount} from '../common';
 
 /**
 * Revolve a loan
@@ -23,11 +23,15 @@ implements CSCoreSDK.UpdateEnabled<TransferRequest, TransferResponse> {
         // transform Date objects to ISO strings
         CSCoreSDK.EntityUtils.transformDatesToSimpleISO('transferDate', payload);
         
-        return CSCoreSDK.ResourceUtils.CallUpdate(this, payload);
+        return CSCoreSDK.ResourceUtils.CallUpdate(this, payload).then(response => {
+            
+            CSCoreSDK.SigningUtils.createSingingObject(<CSCoreSDK.HasSignInfo>response, this.getClient(), this.getPath());
+            return response;
+        })
     }
 }
 
-export interface TransferResponse extends Signable {}
+export interface TransferResponse extends CSCoreSDK.Signable {}
 
 export interface TransferRequest {
     
