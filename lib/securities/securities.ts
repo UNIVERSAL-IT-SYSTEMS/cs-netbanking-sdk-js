f91c5cc0ef2f781f61865e953965c79548a3dcf1
Petr Bažout <bazout.p@gmail.com>
2016-07-29 13:52:32 +0200

    Add interfaces for securities

--- lib/securities/securities.ts
+++ lib/securities/securities.ts
@@ -1,11 +1,12 @@
 /// <reference path="../../node_modules/cs-core-sdk/dist/cs-core-sdk.node.d.ts" />
 import CSCoreSDK = require('cs-core-sdk');
 import {SecurityTransactionsResource} from './transactions';
+import {Amount, Signable} from '../common';
 
 export class SecuritiesResource extends CSCoreSDK.Resource
-implements CSCoreSDK.PaginatedListEnabled<any>, CSCoreSDK.HasInstanceResource<SecurityResource> {
+implements CSCoreSDK.PaginatedListEnabled<Security>, CSCoreSDK.HasInstanceResource<SecurityResource> {
     
-    list = (params?: any): Promise<any> => {
+    list = (params?: SecuritiesParams): Promise<SecurityList> => {
         return CSCoreSDK.ResourceUtils.CallPaginatedListWithSuffix(this, null, 'securitiesAccounts', params, response => {
             return response;
         });
@@ -17,13 +18,13 @@ implements CSCoreSDK.PaginatedListEnabled<any>, CSCoreSDK.HasInstanceResource<Se
 }
 
 export class SecurityResource extends CSCoreSDK.InstanceResource
-implements CSCoreSDK.GetEnabled<any>, CSCoreSDK.UpdateEnabled<any, any> {
+implements CSCoreSDK.GetEnabled<Security>, CSCoreSDK.UpdateEnabled<SecurityRequest, SecurityResponse> {
     
-    get = (): Promise<any> => {
+    get = (): Promise<Security> => {
         return CSCoreSDK.ResourceUtils.CallGet(this, null);
     }
     
-    update = (payload: any): Promise<any> => {
+    update = (payload: SecurityRequest): Promise<SecurityResponse> => {
         return CSCoreSDK.ResourceUtils.CallUpdate(this, payload);
     }
 
@@ -31,3 +32,115 @@ implements CSCoreSDK.GetEnabled<any>, CSCoreSDK.UpdateEnabled<any, any> {
         return new SecurityTransactionsResource(`${this.getPath()}/transactions`, this.getClient());
     }
 }
+
+export interface SecurityList extends CSCoreSDK.PaginatedListResponse<Security> {}
+
+export interface Security {
+
+    /**
+     * Product id
+     */
+    id: string;
+
+    /**
+     * Account identification number of security portfolio (MUIN)
+     */
+    accountno: string;
+
+    /**
+     * Alias for security portfolio. Max. 50 characters.
+     */
+    alias?: string;
+
+    /**
+     * Description - Securities portfolio Account name, Name of principal account holder
+     */
+    description: string;
+
+    /**
+     * Account balance value
+     */
+    balance: Amount;
+
+    /**
+     * Array of securities sub accounts
+     */
+    subSecAccounts?: [SubSecAccount];
+}
+
+// z dokumentace
+export interface SubSecAccount {
+
+    /**
+     * Sub Securities Account ID
+     */
+    id: string;
+
+    /**
+     * Array of the titles within sub account.
+     */
+    titles?: [{
+
+        /**
+         * Name of the security title.
+         */
+        title: string;
+
+        /**
+         * ISIN - identifier of the security title.
+         */
+        isin: string;
+
+        /**
+         * Number of securities/shares
+         */
+        numberOfShares: number;
+
+        /**
+         * Last Price of Securities title
+         */
+        lastPrice: Amount;
+
+        /**
+         * Date of securities last price evaluation
+         */
+        lastPriceDate: Date;
+
+        /**
+         * Market value of the securities title.
+         */
+        marketValue: Amount;
+
+        /**
+         * Security Product Type. Possible values: BOND, SHARE, FUND, IPO, OPTION, OTHER, INDEX, CERTIFICATE, INVESTMENT,KNOCKOUT, UNKNOWN.
+         */
+        securityType: string;
+
+        /**
+         * Security Product Group. Possible values: BONDS_AND_MORE, GUARANTEE_OF_PRINCIPAL, NO_GUARANTEE_OF_PRINCIPAL, REAL_ESTATE, SHARES, STOCK_AND_MIXED, INVESTMENT, KNOCK_OUT, UNKNOWN.
+         */
+        productGroup: string;
+
+        /**
+         * Localized security indication depending on security type and product group
+         */
+        securityIndication: string;
+    }];
+
+    /**
+     * Array of flags.
+     */
+    flags: [string]
+}
+
+export interface SecuritiesParams extends CSCoreSDK.Paginated {}
+
+export interface SecurityRequest {
+
+    /**
+     * Alias for security portfolio. Max. 50 characters.
+     */
+    alias?: string;
+}
+
+export interface SecurityResponse extends Security, Signable {}
--- lib/securities/transactions.ts
+++ lib/securities/transactions.ts
@@ -1,5 +1,6 @@
 /// <reference path="../../node_modules/cs-core-sdk/dist/cs-core-sdk.node.d.ts" />
 import CSCoreSDK = require('cs-core-sdk');
+import {Signable, ExportTransactionsParameters} from '../common';
 
 export class SecurityTransactionsResource extends CSCoreSDK.Resource
 implements CSCoreSDK.HasInstanceResource<SecurityTransactionResource> {
@@ -15,7 +16,7 @@ implements CSCoreSDK.HasInstanceResource<SecurityTransactionResource> {
         return new SecurityTransactionResource(id, this.getPath(), this.getClient());
     }
 
-    export = (params: any): Promise<any> => {
+    export = (params: ExportTransactionsParameters): Promise<any> => {
 
         // transform "fields" parameter to comma separated list from array
         CSCoreSDK.EntityUtils.transformArrayParamsToString(params, 'fields');
@@ -29,10 +30,50 @@ implements CSCoreSDK.HasInstanceResource<SecurityTransactionResource> {
 }
 
 export class SecurityTransactionResource extends CSCoreSDK.InstanceResource
-implements CSCoreSDK.UpdateEnabled<any, any> {
+implements CSCoreSDK.UpdateEnabled<SecurityTransactionRequest, SecurityTransactionResponse> {
 
-    update = (payload: any): Promise<any> => {
+    update = (payload: SecurityTransactionRequest): Promise<SecurityTransactionResponse> => {
         return CSCoreSDK.ResourceUtils.CallUpdate(this, payload);
     }
 
+}
+
+export interface SecurityTransactionRequest {
+
+    // vzít z resourcu?
+    /**
+     * Transaction identifier.
+     */
+    id: string;
+
+    /**
+     * Personal, user specific note for transaction. Max. 4 000 characters.
+     */
+    note?: string;
+
+    /**
+     * List of flags.
+     */
+    flags?: [string];
+}
+
+export interface SecurityTransactionResponse extends Signable {
+
+    transactions: {
+
+        /**
+         * Transaction identifier.
+         */
+        id: string;
+
+        /**
+         * Personal, user specific note for transaction. Max. 4 000 characters.
+         */
+        note?: string;
+
+        /**
+         * List of flags.
+         */
+        flags?: [string];
+    }
 }
