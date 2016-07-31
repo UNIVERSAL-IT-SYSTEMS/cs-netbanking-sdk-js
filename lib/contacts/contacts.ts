41ac90d993f3fc522f6a238ea59e9f869397f20d
Petr Bažout <bazout.p@gmail.com>
2016-07-31 15:19:04 +0200

    Add interfaces for contacts

--- lib/contacts/contacts.ts
+++ lib/contacts/contacts.ts
@@ -2,9 +2,9 @@
 import CSCoreSDK = require('cs-core-sdk');
 
 export class ContactsResource extends CSCoreSDK.Resource
-implements CSCoreSDK.ListEnabled<any>, CSCoreSDK.HasInstanceResource<ContactResource> {
+implements CSCoreSDK.ListEnabled<Contact>, CSCoreSDK.HasInstanceResource<ContactResource> {
 
-    list = (): Promise<any> => {
+    list = (): Promise<ContactList> => {
         return CSCoreSDK.ResourceUtils.CallListWithSuffix(this, null, 'contacts');
     }
 
@@ -14,9 +14,106 @@ implements CSCoreSDK.ListEnabled<any>, CSCoreSDK.HasInstanceResource<ContactReso
 }
 
 export class ContactResource extends CSCoreSDK.InstanceResource
-implements CSCoreSDK.GetEnabled<any> {
+implements CSCoreSDK.GetEnabled<Contact> {
 
-    get = (): Promise<any> => {
+    get = (): Promise<Contact> => {
         return CSCoreSDK.ResourceUtils.CallGet(this, null);
+    }
+}
+
+export interface ContactList extends CSCoreSDK.ListResponse<Contact> {}
+
+export interface Contact {
+
+    /**
+     * Contact ID
+     */
+    id: string;
+
+    /**
+     * Type of contact. ENUM values: ADDRESS, PHONE, EMAIL, FAX.
+     */
+    type: string;
+
+    /**
+     * Contact flags
+     */
+    flags?: [string];
+
+    /**
+     * Contact address
+     */
+    address?: {
+
+        /**
+         * Address type. ENUM values: PERMANENT_RESIDENCE, SECONDARY_RESIDENCE, COMPANY_RESIDENCE, UNKNOWN
+         */
+        type: string;
+
+        /**
+         * Localized name of address type.
+         */
+        typeI18N: string;
+
+        description?: string;
+
+        street: string;
+
+        streetNumber?: string | number;
+
+        buildingApartment?: string | number;
+
+        zipCode: string;
+
+        city: string;
+
+        country: string;
+    }
+
+    /**
+     * Contact phone
+     */
+    phone?: {
+
+        /**
+         * Phone type. ENUM: PRIVATE, COMPANY, UNKNOWN
+         */
+        type: string;
+
+        /**
+         * Localized name of phone type.
+         */
+        typeI18N: string;
+
+        /**
+         * Country calling code as international phone number prefix. E.g.: "0043" or "+43", "00420" or "+420", "00421" or "+421"
+         */
+        countryCallingCode: string;
+
+        /**
+         * Phone number
+         */
+        phoneNumber: string;
+    }
+
+    /**
+     * Primary contact email address
+     */
+    email?: {
+
+        /**
+         * Email type. ENUM values: PRIVATE, COMPANY, UNKNOWN
+         */
+        type: string;
+
+        /**
+         * Localized name of email type.
+         */
+        typeI18N: string;
+
+        /**
+         * Email
+         */
+        email: string;
     }
 }
