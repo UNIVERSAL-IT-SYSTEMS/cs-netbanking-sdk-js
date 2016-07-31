9d3657b44ecd16ab30a820ebc668521115bf716d
Petr Bažout <bazout.p@gmail.com>
2016-07-31 14:48:59 +0200

    Add interfaces to plugins

--- lib/plugins/plugins.ts
+++ lib/plugins/plugins.ts
@@ -1,10 +1,11 @@
 /// <reference path="../../node_modules/cs-core-sdk/dist/cs-core-sdk.node.d.ts" />
 import CSCoreSDK = require('cs-core-sdk');
+import {Amount, AccountNumber} from '../common';
 
 export class PluginsResource extends CSCoreSDK.Resource
-implements CSCoreSDK.PaginatedListEnabled<any>, CSCoreSDK.HasInstanceResource<PluginResource> {
+implements CSCoreSDK.PaginatedListEnabled<Plugin>, CSCoreSDK.HasInstanceResource<PluginResource> {
 
-    list = (params: any): Promise<any> => {
+    list = (params: PluginsParameters): Promise<PluginList> => {
         return CSCoreSDK.ResourceUtils.CallPaginatedListWithSuffix(this, null, 'plugins', params, response => {
             return response;
         });
@@ -16,9 +17,70 @@ implements CSCoreSDK.PaginatedListEnabled<any>, CSCoreSDK.HasInstanceResource<Pl
 }
 
 export class PluginResource extends CSCoreSDK.InstanceResource
-implements CSCoreSDK.UpdateEnabled<any, any> {
+implements CSCoreSDK.UpdateEnabled<UpdatePluginRequest, SignablePlugin> {
 
-    update = (payload: any) => {
+    update = (payload: UpdatePluginRequest): Promise<SignablePlugin> => {
         return CSCoreSDK.ResourceUtils.CallUpdate(this, payload);
     }
 }
+
+export interface PluginList extends CSCoreSDK.PaginatedListResponse<Plugin> {}
+
+export interface Plugin extends UpdatePluginRequest {
+
+    /**
+     * Localized name of the plugin.
+     */
+    name: string;
+
+    /**
+     * Date until plugin is valid.
+     */
+    validUntil: Date;
+
+    /**
+     * Date of activation of plugin for user.
+     */
+    dateOfActivation?: Date;
+
+    standardFees?: [{
+
+        /**
+         * Time moment of changing the plugin fee. Possible values are IMMEDIATELY, ACCOUNT_STATEMENT, UNKNOWN.
+         */
+        timeOfCharging: string;
+
+        /**
+         * Frequency period of changing the plugin fee. Possible values are MONTHLY, NON_RECURRING, UNKNOWN.
+         */
+        periodOfCharging: string;
+
+        /**
+         * Fee amount defined for this plugin.
+         */
+        amount: Amount;
+    }];
+}
+
+
+export interface UpdatePluginRequest {
+
+    /**
+     * Plugin unique identifier.
+     */
+    productCode: string;
+
+    /**
+     * User settlement account for charging fees.
+     */
+    settlementAccount?: AccountNumber;
+
+    /**
+     * Array of optional flag values.
+     */
+    flags?: [string];   
+}
+
+export interface PluginsParameters extends CSCoreSDK.Paginated {}
+
+export interface SignablePlugin extends Plugin, CSCoreSDK.Signable {}
