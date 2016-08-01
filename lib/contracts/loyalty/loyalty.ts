a4ac055d7c8e36664105e217a5a632d54e66e66f
Petr Bažout <bazout.p@gmail.com>
2016-08-01 15:08:23 +0200

    Add loyalty contracts

---
+++ lib/contracts/loyalty/loyalty.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../../node_modules/cs-core-sdk/dist/cs-core-sdk.node.d.ts" />
+import CSCoreSDK = require('cs-core-sdk');
+
+export class LoyaltyContractsResource extends CSCoreSDK.Resource
+implements CSCoreSDK.GetEnabled<Loyalty> {
+
+    get = (): Promise<Loyalty> => {
+        return CSCoreSDK.ResourceUtils.CallGet(this, null);
+    }
+}
+
+export interface Loyalty {
+
+    /**
+     * State of the ibod account. Possible values are REGISTERED, UNREGISTERED, DEACTIVATED_FROM_FSCS.
+     */
+    state: string;
+
+    /**
+     * Date when data were actual.
+     */
+    exportDate: Date;
+
+    /**
+     * IBod points count.
+     */
+    pointsCount: number;
+
+    /**
+     * Activation ibod code.
+     */
+    activationCode?: string; 
+}
